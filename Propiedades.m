eee00488a24d66eaf8ac9798dfdb3e3fe6a99023
Laura Valencia <lvalenciau@unal.edu.co>
2023-03-19 22:41:34 -0500

    adición archivo propiedades

---
+++ Propiedades.m
@@ -0,0 +1,41 @@
+%CONDICIONES DEL SISTEMA
+T = 150
+P = 101.3
+Tref = 25
+r = 8.314
+
+%PROPIEDADES AGUA
+href = -286031.3
+sref = 69.95
+Teb = 373.15
+hfg = 40653
+cpl = 75.362
+    %Constantes
+    a1 = 31.368736
+    b1 = 6.30E-03
+    c1 = 4.00E-06
+cpg1 = a1 + b1*T + c1*T^2
+
+%PROPIEDADES HIDRÓGENO
+href = 0
+sref = 130.59
+Teb = 
+hfg = 
+cpl = 
+    %Constantes
+    a2 = 
+    b2 = 
+    c2 = 
+cpg2 = a2 + b2*T + c2*T^2
+
+%PROPIEDADES OXÍGENO
+href = 0
+sref = 205.14
+Teb = 
+hfg = 
+cpl = 
+    %Constantes
+    a3 = 
+    b3 = 
+    c3 = 
+cpg3 = a3 + b3*T + c3*T^2
